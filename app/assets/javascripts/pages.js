c6dde85939436c0f4fa1256a32e0c62960fca995
Matt Cameron <mcameron@avalanche.com.au>
2015-05-28 23:46:36 +1000

    added style to the front page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,15 +1,6 @@
-// $('.set-task > input').on('focus', function() {
-		$('.set-deadline').show();
-// });
-
-// $('.set-deadline > input').on('focus', function() {
-		$('.set-bounty').show();
-		$('.create-task-btn').show();
-// });
-
-$(function () {
-  $('.datetimepicker').datetimepicker({
-  		minDate: moment(Date.today),
-      sideBySide: true
-  });
+$('.datetimepicker').datetimepicker({
+		minDate: moment(Date.today),
+    sideBySide: true
 });
+
+$('#task_title').focus();
